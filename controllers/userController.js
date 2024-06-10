bc5a6b864d39097d57284ae51f2c5425e232b3ed
nerahubswebdev <nerahubwebdevelopment@gmail.com>
2024-06-10 11:40:12 +0100

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,13 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 // register business logic
 const register = async (req, res) => {
   try {
     // lets get the body from the request
     const { name, email, bio, password } = req.body;
+    const body = req.body;
 
     // check if required user details are available
     if (!name || !email || !bio || !password) {
@@ -15,6 +18,7 @@ const register = async (req, res) => {
       return;
     }
 
+    // check if the email already exists
     const userexists = await User.findOne({ email: email }).exec();
 
     if (userexists) {
@@ -27,16 +31,116 @@ const register = async (req, res) => {
 
     console.log("the user = ", userexists);
 
-    await User.create(req.body);
+    //encrypting the password
+    const salt = await bcrypt.genSalt(15);
+    const encryptedpassword = await bcrypt.hash(password, salt);
 
-    res.status(200).json({
+    const newuser = await User.create({
+      name,
+      email,
+      bio,
+      password: encryptedpassword,
+    });
+
+    res.status(201).json({
       success: true,
-      message: "The login endpoint hit..",
-      // body: body,
+      message: "Registered successfully..",
+      // user: newuser,
     });
   } catch (error) {
-    console.log("the error => ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
 };
 
-export { register };
+// login business logic
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // cheking if email and password is sent
+    if (!email || !password) {
+      res.status(400).json({
+        success: false,
+        message: "Required fields needed",
+      });
+      return;
+    }
+
+    // check if the email already exists
+    const userexists = await User.findOne({ email: email }).exec();
+
+    if (!userexists) {
+      res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+      return;
+    }
+
+    // compare passwords and check if the password is a valid password
+
+    const validPassword = await bcrypt.compare(password, userexists.password);
+
+    if (!validPassword) {
+      res.status(409).json({
+        success: false,
+        message: "Invalid password",
+      });
+      return;
+    }
+
+    // //TODO: sign a jwt token and produce cookies for access and refresh
+    // create an access token
+    const accessToken = jwt.sign(
+      {
+        access1: userexists?._id,
+      },
+      process.env.jwt_secret,
+      {
+        expiresIn: process.env.accesstime,
+      }
+    );
+
+    const refreshToken = jwt.sign(
+      {
+        access1: userexists?._id,
+      },
+      process.env.refresh_secret,
+      {
+        expiresIn: process.env.refreshtime,
+      }
+    );
+
+    // console.log("the access token => ", accessToken);
+
+    //the cookies
+    res.cookie("access", accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 1 * 60 * 1000,
+    });
+
+    res.cookie("refresh", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 5 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful.",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export { register, login };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { register } from "../controllers/userController.js";
+import { login, register } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/create", register);
-//router.post("/login", login);
+router.post("/login", login);
 
 export default router;
