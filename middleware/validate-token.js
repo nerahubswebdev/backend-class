13d34dd0e68d99ca0b23357055f51f3c50b8fb8a
NeraHubs <167062725+nerahubswebdev@users.noreply.github.com>
2024-11-14 11:34:55 +0100

    Update validate-token.js

--- middleware/validate-token.js
+++ middleware/validate-token.js
@@ -60,7 +60,7 @@ export const checkAndRenewToken = (req, res, next) => {
               httpOnly: true,
               secure: true,
               sameSite: "none",
-              maxAge: 20 * 60 * 60 * 1000,
+              maxAge: 20 * 1000,
             });
 
             // assignment to look up what the destructuring means
